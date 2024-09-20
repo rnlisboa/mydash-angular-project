49515a6b8d4e4b35db14472ee3fc438ac240677a
rnlisboa <dev.renanlisboa@gmail.com>
2024-09-20 18:35:08 -0300

    chore: remove inutilizaveis

--- src/app/shared/components/layout/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from "@angular/core";
-
-@Component({
-    selector: 'header-avatar',
-    templateUrl: './header.component.html'
-})
-export class HeaderComponent {
-    
-}
