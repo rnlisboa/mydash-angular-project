8ed60a6d31e77c441bcbcbb69c5c4fb08dad1a58
rnlisboa <dev.renanlisboa@gmail.com>
2024-09-21 21:47:01 -0300

    refactor: refatoração da sidebar

--- src/app/shared/components/layout/sidebar/sidebar/sidebar.component.ts
+++ src/app/shared/components/layout/sidebar/sidebar/sidebar.component.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Component({
     selector: 'sidebar-principal',
     templateUrl: './sidebar.component.html'
 })
-export class SidebarPrincipal implements OnInit {
+export class SidebarPrincipal  {
     sidebarOpen = true;
 
     constructor(public router: Router) { }
 
-    ngOnInit(): void {
-        console.log(this.router)
-    }
-
     isActive(route: string): boolean {
-        console.log(this.router.url)
         return this.router.url === route;
     }
 
