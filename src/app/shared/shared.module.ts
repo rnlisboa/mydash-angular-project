1ee6341160d3341453d2dbec39bafcd3cb8abc7d
rnlisboa <dev.renanlisboa@gmail.com>
2024-09-21 21:48:58 -0300

    feat: utilização de novos modulos da aplicação

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { LayoutDashboardIcon, LayoutList, LucideAngularModule, Menu, Search,  } from 'lucide-angular'
 import { HeaderLayout } from './components/layout/header-layout/header-layout.component'; 
 import { SidebarPrincipal } from './components/layout/sidebar/sidebar/sidebar.component';
 import { HeaderSidebar } from './components/layout/sidebar/header/header.component';
 import { TopPageComponent } from './components/layout/top-page/top-page.component';
+import { BreadcrumbComponent } from './components/layout/breadcrumb/breadcrumb.componente';
+import { RouterModule } from '@angular/router';
+import { ButtonSelectorComponent } from './components/layout/button/button.component';
+import { DatatableComponent } from './components/tasks/datatable/datatable.component';
+import { DialogModule } from 'primeng/dialog';
+import { ModalComponent } from './components/layout/modal/modal.component';
+
 
 @NgModule({
   declarations: [
@@ -12,15 +20,25 @@ import { TopPageComponent } from './components/layout/top-page/top-page.componen
     SidebarPrincipal,
     HeaderSidebar,
     TopPageComponent,
+    BreadcrumbComponent,
+    ButtonSelectorComponent,
+    DatatableComponent,
+    ModalComponent,
   ],
   imports: [
     CommonModule,
+    RouterModule,
+    
     LucideAngularModule.pick({Menu, LayoutList, LayoutDashboardIcon, Search})
   ],
   exports: [
     HeaderLayout,
     SidebarPrincipal,
     TopPageComponent,
+    BreadcrumbComponent,
+    ButtonSelectorComponent,
+    DatatableComponent,
+    ModalComponent,
   ]
 })
 export class SharedModule {}
