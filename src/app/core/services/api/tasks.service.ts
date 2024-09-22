026aa4d516ce421ee6300ebe6dd02969980b7d36
rnlisboa <dev.renanlisboa@gmail.com>
2024-09-22 16:12:51 -0300

    feat: implementa criação de tarefas

--- src/app/core/services/api/tasks.service.ts
+++ src/app/core/services/api/tasks.service.ts
@@ -2,19 +2,27 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { TaskModel } from "./model/task.model";
 import { Observable } from "rxjs";
+import { UserModel } from "./model/user.model";
 
 @Injectable({
     providedIn: 'root'
-})
-export class TaskService {
-    private apiUrl = 'http://localhost:3000/tasks'
-    constructor(private http: HttpClient) {
-
+  })
+  export class TaskService {
+    private apiUrl = 'http://localhost:3000/tasks';
+    private userUrl = 'http://localhost:3000/users';  // Endpoint para buscar usuários
+  
+    constructor(private http: HttpClient) { }
+  
+    getAll(): Observable<TaskModel[]> {
+      return this.http.get<TaskModel[]>(this.apiUrl);
     }
-
-    getAll(): Observable<TaskModel[]>{
-        return this.http.get<TaskModel[]>(this.apiUrl);
+  
+    searchUsers(term: string): Observable<UserModel[]> {
+      return this.http.get<UserModel[]>(`${this.userUrl}?name_like=${term}`);
     }
-
-
-}
+  
+    createTask(task: TaskModel): Observable<TaskModel> {
+      return this.http.post<TaskModel>(this.apiUrl, task);
+    }
+  }
+  
--- src/app/shared/components/layout/modal/modal.component.ts
+++ src/app/shared/components/layout/modal/modal.component.ts
@@ -1,22 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalStateService } from '../../../../core/services/state/state-modal.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { TaskService } from '../../../../core/services/api/tasks.service';
+import { UserModel } from '../../../../core/services/api/model/user.model';
 
 @Component({
-    selector: 'modal',
-    templateUrl: './modal.component.html'
+  selector: 'modal',
+  templateUrl: './modal.component.html'
 })
 export class ModalComponent implements OnInit {
-    visible: boolean = false;
+  visible: boolean = false;
+  taskForm: FormGroup;
+  filteredUsers: UserModel[] = [];
 
-    constructor(private modalStateService: ModalStateService) { }
+  constructor(
+    private modalStateService: ModalStateService,
+    private taskService: TaskService,
+    private fb: FormBuilder
+  ) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      detail: ['', Validators.required],
+      deadline: ['', Validators.required],
+      priority: ['', Validators.required],
+      responsible: ['', Validators.required],
+      finished: [false]
+    });
+  }
 
-    ngOnInit() {
-        this.modalStateService.modalVisible$.subscribe(isVisible => {
-            this.visible = isVisible;
-        });
+  ngOnInit() {
+    this.modalStateService.modalVisible$.subscribe(isVisible => {
+      this.visible = isVisible;
+    });
+
+    // Responsável search
+    this.taskForm.get('responsible')?.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(value => this.taskService.searchUsers(value))
+    ).subscribe(users => {
+      this.filteredUsers = users;
+    });
+  }
+
+  hideDialog() {
+    this.modalStateService.closeModal();
+  }
+
+  selectUser(user: UserModel) {
+  this.taskForm.patchValue({ responsible: user.name });
+  this.filteredUsers = []; // Esvazia a lista para que ela desapareça
+}
+
+
+  onSubmit() {
+    if (this.taskForm.valid) {
+      const task = this.taskForm.value;
+      this.taskService.createTask(task).subscribe(response => {
+        console.log('Tarefa criada com sucesso!', response);
+        this.hideDialog();
+      });
     }
-
-    hideDialog() {
-        this.modalStateService.closeModal();
-    }
+  }
 }
